8979840df5f7caa35dfcd852f017adb6038fd524
Sadashiv <65489912+prompt-07@users.noreply.github.com>
2022-01-02 20:06:21 +0530

    conn string change

--- Database Connection file/mysqli_connect.php
+++ Database Connection file/mysqli_connect.php
@@ -2,7 +2,7 @@
 DEFINE('DB_USER','root');
 DEFINE('DB_PASSWORD','');
 DEFINE('DB_HOST','localhost');
-DEFINE('DB_NAME','airline_reservation');
+DEFINE('DB_NAME','ticket_reservation');//airline_reservation
 
 $dbc=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)
 OR dies('Could not connect to MySQL:' .
